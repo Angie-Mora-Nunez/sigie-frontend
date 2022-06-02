decaf721ce406a8f5506aabd79373a31783db068
Angie Mora Núñez <52469029+Angie-Mora-Nunez@users.noreply.github.com>
2022-06-02 14:29:57 -0600

    Agregando app-angular

---
+++ sigie-frontend/src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SigieHeaderComponent } from './sigie-header/sigie-header.component';
+
+
+
+@NgModule({
+  declarations: [
+    SigieHeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
